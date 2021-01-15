6c6915462f317ab1b3d7cd1ad2a20096dcb0a90c
Ajinkya Patil <san.avpatil@gmail.com>
2021-01-15 18:56:23 +0530

    My First app

---
+++ src/myfirstapp/utility/utility.go
@@ -0,0 +1,13 @@
+package utility
+
+import "fmt"
+
+// SayHello ...
+func SayHello() {
+	fmt.Println("Hello World. This is my first app")
+}
+
+// SayThanks ...
+func SayThanks() {
+	fmt.Println("Thanks for checking my first app")
+}
