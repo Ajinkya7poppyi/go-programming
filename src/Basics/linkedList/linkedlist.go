674b426b10ac84a2a30bd1252f3e19d5b7c3268f
Ajinkya Patil <san.avpatil@gmail.com>
2021-01-16 12:19:38 +0530

    Linked list

--- src/Basics/linkedList/linkedlist.go
+++ src/Basics/linkedList/linkedlist.go
@@ -23,8 +23,43 @@ func NewSLLNode() *SLLNode {
 	return new(SLLNode)
 }
 
+// SingleLinkedList ...
+type SingleLinkedList struct {
+	head *SLLNode
+	tail *SLLNode
+}
+
+func newSingleLinkedList() *SingleLinkedList {
+	return new(SingleLinkedList)
+}
+
+//Add ...
+func (list *SingleLinkedList) Add(v int) {
+	newNode := &SLLNode{value: v}
+	if list.head == nil {
+		list.head = newNode
+	} else if list.tail == list.head {
+		list.head.next = newNode
+	} else if list.tail != nil {
+		list.tail.next = newNode
+	}
+	list.tail = newNode
+}
+
+//String ..
+func (list *SingleLinkedList) String() string {
+	s := ""
+	for n := list.head; n != nil; n = n.next {
+		s += fmt.Sprintf(" {%d} ", n.GetValue())
+	}
+	return s
+}
+
 func main() {
-	node := NewSLLNode()
-	node.SetValue(4)
-	fmt.Println("Node is of value:", node.GetValue())
+	list := newSingleLinkedList()
+	list.Add(3)
+	list.Add(5)
+	list.Add(2)
+	list.Add(3)
+	fmt.Println("Hello, Lists", list)
 }
